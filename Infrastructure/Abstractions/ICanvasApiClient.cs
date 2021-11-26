35c89fa69a1940834d8ff9bfc6d48781196968cf
Andre Burke <aburke.dev@gmail.com>
2021-11-26 00:01:05 -0700

    added method for getting notifications

--- Infrastructure/Abstractions/ICanvasApiClient.cs
+++ Infrastructure/Abstractions/ICanvasApiClient.cs
@@ -6,4 +6,5 @@ public interface ICanvasApiClient
 {
     Task<IReadOnlyList<Course>> GetCoursesAsync(CancellationToken ct = default);
     Task<string> GetScopesAsync(CancellationToken ct = default);
+    Task<string> GetNotificationsAsync(long accountId, CancellationToken ct = default);
 }
